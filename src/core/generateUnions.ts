b7b4fd8c0369eed2ef26fd2cd2d51d54d4fc718a
John Yuan <john.ycq@outlook.com>
2024-04-07 10:53:41 +0800

    feat: use single quote for symbol names

--- src/core/generateUnions.ts
+++ src/core/generateUnions.ts
@@ -1,7 +1,10 @@
 export function generateUnions(typeName: string, unions: string[]) {
   const code: string[] = [`export type ${typeName} =`]
+  const formatName = (name: string) => {
+    return name.includes(`'`) ? JSON.stringify(name) : `'${name}'`
+  }
 
-  unions.forEach((name) => code.push(`  | ${JSON.stringify(name)}`))
+  unions.forEach((name) => code.push(`  | ${formatName(name)}`))
 
   return code.join('\n')
 }
